dafd89da7b236816c25e97f769bbf6af46122dae
kasia.kordys <kasia.kordys@gmail.com>
2022-02-20 17:34:21 +0200

    add transition

--- angular-animations/src/app/app.component.ts
+++ angular-animations/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
 
 @Component({
   selector: 'app-root',
-  template: `<button [@myTrigger]='state'>My button</button>`,
+  template: `<button [@myTrigger]='state' (click)='toggleState()'>My button</button>`,
   styles: [],
   animations: [
     trigger('myTrigger', [
@@ -12,13 +12,19 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
       })),
       state('large', style({
         transform: 'scale(1.4)'
-      }))
-      //transition
+      })),
+      // transition('small => large', animate('500ms ease-in')),
+      // transition('large => small', animate('500ms ease-out'))
+      transition('large <=> small', animate('500ms ease-out'))
     ])
 
   ]
 })
 export class AppComponent {
-  state: string = "large";
+  state: string = "small";
   title = 'angular-animations';
+  toggleState() {
+    this.state = (this.state === 'small' ? 'large': 'small');
+  }
+
 }
